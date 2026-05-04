9b5bfd49d771ee2fa20c0abf9056b29559a47652
romankilchenmann4-eng <romankilchenmann4@gmail.com>
2026-05-04 19:53:49 +0200

    Delete src directory

--- src/lib/stripe.ts
+++
@@ -1,7 +0,0 @@
-import Stripe from "stripe";
-export function getStripe() {
-  return new Stripe(process.env.STRIPE_SECRET_KEY ?? "sk_test_placeholder", {
-    apiVersion: "2025-02-24.acacia",
-    typescript: true,
-  });
-}
