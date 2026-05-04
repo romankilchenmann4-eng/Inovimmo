89c372e501c12d4b3e2fc93614576688985db8c6
romankilchenmann4-eng <romankilchenmann4@gmail.com>
2026-05-04 16:43:43 +0200

    Update stripe.ts

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,8 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-02-24.acacia",
-  typescript: true,
-});
+export function getStripe() {
+  return new Stripe(process.env.STRIPE_SECRET_KEY ?? "sk_test_placeholder", {
+    apiVersion: "2025-02-24.acacia",
+    typescript: true,
+  });
+}
