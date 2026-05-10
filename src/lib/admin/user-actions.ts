7fd5dc9d906c8ca26dbe2f5c504697a9cb10c8ad
romankilchenmann4-eng <romankilchenmann4@gmail.com>
2026-05-10 16:35:18 +0200

    Update user-actions.ts

--- src/lib/admin/user-actions.ts
+++ src/lib/admin/user-actions.ts
@@ -23,7 +23,7 @@ export async function adminCreateUser(formData: FormData) {
   // =========================================================
   // Benutzer erstellen
   // =========================================================
-
+r
   const { data, error } =
     await supabaseAdmin.auth.admin.createUser({
       email,
@@ -72,6 +72,6 @@ export async function adminCreateUser(formData: FormData) {
   }
 
   return {
-    success: true,
+    success:
   };
 }
